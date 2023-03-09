38bc99c8e5b56fdd5380298cd70b677dd54b5797
Carter Norton <cnorton360@gmail.com>
2023-03-09 11:31:25 -0700

    Finished basic function of routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+
+routes = {};
 class Route{
     constructor(name,area,aspect,approach,vert){
         this.name = name;
@@ -6,6 +8,8 @@ class Route{
         this.approach = approach;
         this.vert = vert;
         this.id = (Math.round(Math.random() * 10000)).toString();
+
+        routes[this.id] = this;
     }
 
     getHTML(){
@@ -15,7 +19,7 @@ class Route{
         newRoute.innerHTML =
             `
             <h2 class="accordion-header" id="heading${this.id}">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${this.id}" aria-expanded="true" aria-controls="collapse${this.id}">
+                <button id = "${this.id}headingButton" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${this.id}" aria-expanded="true" aria-controls="collapse${this.id}">
                     ${this.name}
                 </button>
             </h2>
@@ -26,24 +30,26 @@ class Route{
                             <tbody>
                             <tr>
                                 <td>Area</td>
-                                <td>${this.area}</td>
+                                <td id = "${this.id}area">${this.area}</td>
                             </tr>
                             <tr>
                                 <td> Aspect </td>
-                                <td>${this.aspect}</td>
+                                <td id = "${this.id}aspect">${this.aspect}</td>
                             </tr>
                             <tr>
                                 <td>Approach</td>
-                                <td>${this.approach}</td>
+                                <td id = "${this.id}approach">${this.approach}</td>
                             </tr>
                             <tr>
                                 <td>Vert</td>
-                                <td>${this.vert}</td>
+                                <td id = "${this.id}vert">${this.vert}</td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
-                    <button class="btn btn-primary" href="#" style="right: 0%">edit</button>
+                    <div id = "${this.id}editSpace">
+                        <button class="btn btn-primary" href="#" style="right: 0%" onclick="editRoute(${this.id})">edit</button>
+                    </div>
                 </div>
             </div>
             `
@@ -51,10 +57,96 @@ class Route{
 
     }
 }
-function testerFunction() {
-    const dummyRoute = new Route("Route 1", "Area 1", "Aspect 1", "1 mile", "1000");
-
-    console.log("called test function!");
+function createNew() {
+    const dummyRoute = new Route("", "", "", "", "");
     const accord = document.getElementById("accordionExample");
     accord.appendChild(dummyRoute.getHTML());
+    editRoute(dummyRoute.id);
 }
+
+function editRoute(id){
+
+    let editSpace = document.getElementById(id + "editSpace");
+    let headingButton = document.getElementById(id + "headingButton");
+    let areaTD = document.getElementById(id + "area");
+    let aspectTD = document.getElementById(id + "aspect");
+    let approachTD = document.getElementById(id + "approach");
+    let vertTD = document.getElementById(id + "vert");
+
+    //Access the prior information
+
+    areaTD.innerHTML =
+        `
+        <input type="text" id = "${id}areaEdit" value = "${routes[id].area}">
+        `;
+    aspectTD.innerHTML =
+        `
+        <input type="text" id = "${id}aspectEdit" value = "${routes[id].aspect}">
+        `;
+    approachTD.innerHTML =
+        `
+        <input type="text" id = "${id}approachEdit" value = "${routes[id].approach}">
+        `;
+    vertTD.innerHTML =
+        `
+        <input type="text" id = "${id}vertEdit" value = "${routes[id].vert}">
+        `;
+
+    editSpace.innerHTML=
+        `
+         <button class="btn btn-danger" style="right: 0%" onclick = "cancelEdit(${id})">cancel</button>
+         <button class="btn btn-primary" style="right: 0%" onclick = "saveEdit(${id})">save</button>
+        `;
+    headingButton.innerHTML=
+        `
+        <input type="text" id = "${id}nameEdit" value = "${routes[id].name}">
+        `
+}
+
+function cancelEdit(id){
+    route = routes[id];
+
+    let editSpace = document.getElementById(id + "editSpace");
+    let headingButton = document.getElementById(id + "headingButton");
+    let areaTD = document.getElementById(id + "area");
+    let aspectTD = document.getElementById(id + "aspect");
+    let approachTD = document.getElementById(id + "approach");
+    let vertTD = document.getElementById(id + "vert");
+
+    //reset information
+
+    areaTD.innerHTML = `${route.area}`;
+    headingButton.innerHTML = `${route.name}`;
+    aspectTD.innerHTML = `${route.aspect}`;
+    approachTD.innerHTML = `${route.approach}`;
+    vertTD.innerHTML = `${route.vert}`;
+
+    editSpace.innerHTML =
+        `
+         <button class="btn btn-primary" href="#" style="right: 0%" onclick="editRoute(${id})">edit</button>
+        `
+
+}
+
+function saveEdit(id){
+    route = routes[id];
+
+    let areaTD = document.getElementById(id + "areaEdit");
+    let aspectTD = document.getElementById(id + "aspectEdit");
+    let approachTD = document.getElementById(id + "approachEdit");
+    let vertTD = document.getElementById(id + "vertEdit");
+    let headingButton = document.getElementById(id + "nameEdit");
+
+    //update route info
+    route.area = areaTD.value;
+    route.aspect = aspectTD.value;
+    route.approach = approachTD.value;
+    route.vert = vertTD.value;
+    route.name = headingButton.value;
+
+    routes[id] = route;
+
+    cancelEdit(id);
+
+}
+
